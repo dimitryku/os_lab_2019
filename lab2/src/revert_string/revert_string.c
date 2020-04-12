ed951396fcd9769292b9b5fba1db8d5f1afcc61b
dimitryku <diman.kuzmin347@gmail.com>
2020-04-13 00:52:01 +0300

    Решение в solution.md, в 1 и 2 код просто вставлен в соотвтествующие файлы

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include "stdio.h"
+
 
 void RevertString(char *str)
 {
-	// your code here
+	int stl = strlen(str);
+	char a;
+	for (int i = 0; i < stl/2; i++)
+	{
+		a = str[i];
+		str[i] = str[stl - i - 1];
+		str[stl - i - 1] = a;
+	}
 }
 
