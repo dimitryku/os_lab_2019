ffc4499a97d4a1d78ab0775f1249b96fd3ff3571
dimitryku <diman.kuzmin347@gmail.com>
2020-05-08 11:52:33 +0000

    close to finish 5

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
 #include <pthread.h>
+
+#include "find_min_max.h"
 
 struct SumArgs {
   int *array;
@@ -13,6 +19,8 @@ struct SumArgs {
 int Sum(const struct SumArgs *args) {
   int sum = 0;
   // TODO: your code here 
+  for(int i = args->begin; i < args->end; i++)
+    sum += (args->array)[i];
   return sum;
 }
 
@@ -34,6 +42,72 @@ int main(int argc, char **argv) {
   uint32_t seed = 0;
   pthread_t threads[threads_num];
 
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"seed", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {"threads_num", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "f", options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+      case 0:
+        switch (option_index) {
+          case 0:
+            seed = atoi(optarg);
+            if (seed <= 0) {
+                printf("seed is a positive number\n");
+                return 1;
+            }
+            break;
+          case 1:
+            array_size = atoi(optarg);
+            if (array_size <= 0) {
+                printf("array_size is a positive number\n");
+                return 1;
+            }
+            break;
+          case 2:
+            threads_num = atoi(optarg);
+            if (threads_num <= 0) {
+                printf("threads_num is a positive number\n");
+                return 1;
+            }
+            if(threads_num > array_size){
+                printf("threads_num must be less than array_size\n");
+                threads_num = array_size;
+                printf("threads_num is equal to array_size now (%d)\n", array_size);
+            }
+            break;
+
+          defalut:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (optind < argc) {
+    printf("Has at least one no option argument\n");
+    return 1;
+  }
+
+  if (seed == -1 || array_size == -1 || threads_num == -1) {
+    printf("Usage: %s --seed \"num\" --array_size \"num\" --threads_num \"num\" \n", argv[0]);
+    return 1;
+  }
+
   /*
    * TODO:
    * your code here
@@ -41,8 +115,17 @@ int main(int argc, char **argv) {
    */
 
   int *array = malloc(sizeof(int) * array_size);
+  GenerateArray(array, array_size, seed);
 
   struct SumArgs args[threads_num];
+  for(uint32_t i = 0; i < threads_num; i++)
+  {
+      args[i].array = array;
+      args[i].begin = array_size/threads_num*i;
+      args[i].end = array_size/threads_num*(i+1);
+  }
+
+
   for (uint32_t i = 0; i < threads_num; i++) {
     if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
       printf("Error: pthread_create failed!\n");
