d236ab25ebfd3ab3f6700711812738c77b992d83
dimitryku <diman.kuzmin347@gmail.com>
2020-05-20 20:22:33 +0000

    some code goes to separate .c

--- lab6/src/server.c
+++ lab6/src/server.c
@@ -11,6 +11,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include "factorial.h"
 #include "pthread.h"
 
 struct FactorialArgs {
@@ -18,19 +19,6 @@ struct FactorialArgs {
   uint64_t end;
   uint64_t mod;
 };
-
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-  uint64_t result = 0;
-  a = a % mod;
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
-
-  return result % mod;
-}
 
 uint64_t Factorial(const struct FactorialArgs *args) {
   uint64_t ans = 1;
