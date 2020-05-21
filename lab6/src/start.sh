dc51f3200b8c525b82bef8cf8f3554917a184362
dimitryku <diman.kuzmin347@gmail.com>
2020-05-21 12:38:40 +0000

    starting script

---
+++ lab6/src/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#nserv nthreads k mod addres
+
+touch $5
+chmod 777 $5
+I=0
+cat /dev/null > $5
+while [ $I -lt $1 ]
+do
+./server --port $[20001+$I] --tnum $2 $
+echo localhost: $[20001+$I] >> $5
+echo server started
+I=$[ $I + 1 ]
+done
+./client --k $3 --mod $4 --servers $5 $
+echo client started
