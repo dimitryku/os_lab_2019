69d95b94ccc21fa0a9698e25800bce22643d9dd8
dimitryku <diman.kuzmin347@gmail.com>
2020-05-26 16:16:24 +0000

    first changes

--- lab7/src/tcpclient.c
+++ lab7/src/tcpclient.c
@@ -6,20 +6,28 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <getopt.h>
 
-#define BUFSIZE 100
 #define SADDR struct sockaddr
 #define SIZE sizeof(struct sockaddr_in)
 
 int main(int argc, char *argv[]) {
   int fd;
   int nread;
-  char buf[BUFSIZE];
+  int buffsize = atoi(argv[3]);
+  int size = 0;
   struct sockaddr_in servaddr;
-  if (argc < 3) {
+
+  
+    if (argc < 4) {
     printf("Too few arguments \n");
+    printf("usage %s ip port buffsize\n", argv[0]);
     exit(1);
   }
+
+  char buf[buffsize];
+
 
   if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("socket creating");
@@ -42,7 +50,7 @@ int main(int argc, char *argv[]) {
   }
 
   write(1, "Input message to send\n", 22);
-  while ((nread = read(0, buf, BUFSIZE)) > 0) {
+  while ((nread = read(0, buf, buffsize)) > 0) {
     if (write(fd, buf, nread) < 0) {
       perror("write");
       exit(1);
